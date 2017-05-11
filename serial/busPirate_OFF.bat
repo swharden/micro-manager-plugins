85100c3bc1b388a9d22b745c87c74b6a02374905
Scott W Harden <swharden@gmail.com>
2017-05-10 22:48:35 -0400

    1-wire mode works best

--- serial/busPirate_OFF.bat
+++ serial/busPirate_OFF.bat
@@ -2,4 +2,4 @@
 
 echo "TURNING LED OFF"
 
-powershell "$port=new-Object System.IO.Ports.SerialPort COM3,115200,None,8,one; $port.open(); $port.WriteLine('#'); $port.Close()"
+powershell "$port=new-Object System.IO.Ports.SerialPort COM3,115200,None,8,one; $port.open(); $port.WriteLine('w'); $port.ReadLine();  $port.Close()"
--- serial/busPirate_ON.bat
+++ serial/busPirate_ON.bat
@@ -2,4 +2,4 @@
 
 echo "TURNING LED ON"
 
-powershell "$port=new-Object System.IO.Ports.SerialPort COM3,115200,None,8,one; $port.open(); $port.WriteLine('#'); $port.ReadLine(); $port.WriteLine('m'); $port.ReadLine(); $port.WriteLine('2'); $port.ReadLine(); $port.WriteLine('3'); $port.ReadLine(); $port.WriteLine('W'); $port.ReadLine(); $port.Close()"
+powershell "$port=new-Object System.IO.Ports.SerialPort COM3,115200,None,8,one; $port.open(); $port.WriteLine('#'); $port.ReadLine(); $port.WriteLine('m'); $port.ReadLine(); $port.WriteLine('2'); $port.WriteLine('W'); $port.ReadLine(); $port.Close()"
